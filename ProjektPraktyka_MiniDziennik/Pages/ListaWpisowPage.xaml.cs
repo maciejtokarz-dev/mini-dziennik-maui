90e1e1efd2067de28c287f769987032961ba861d
Maciek <maciektbb@gmail.com>
2026-09-18 13:46:13 +0200

    Dodanie plików code-behind i zmiany kosmetyczne

---
+++ ProjektPraktyka_MiniDziennik/Pages/ListaWpisowPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektPraktyka_MiniDziennik.Pages
+{
+    public partial class ListaWpisowPage : ContentPage
+    {
+        public ListaWpisowPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ ProjektPraktyka_MiniDziennik/Pages/StronaGlownaPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektPraktyka_MiniDziennik.Pages
+{
+    public partial class StronaGlownaPage : ContentPage
+    {
+        public StronaGlownaPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
