619838600a7ddaf0b38f6c80249f0554a0c362b5
Maciek <maciektbb@gmail.com>
2026-09-21 12:15:57 +0200

    Naprawienie błędów przy powrocie do listy notatek.

--- ProjektPraktyka_MiniDziennik/AppShell.xaml.cs
+++ ProjektPraktyka_MiniDziennik/AppShell.xaml.cs
@@ -6,7 +6,6 @@
         {
             InitializeComponent();
 
-            Routing.RegisterRoute(nameof(Pages.StronaGlownaPage), typeof(Pages.StronaGlownaPage));
             Routing.RegisterRoute(nameof(Pages.ListaWpisowPage), typeof(Pages.ListaWpisowPage));
             Routing.RegisterRoute(nameof(Pages.DodajEdytujWpisPage), typeof(Pages.DodajEdytujWpisPage));
             Routing.RegisterRoute(nameof(Pages.StatystykiWagiPage), typeof(Pages.StatystykiWagiPage));
